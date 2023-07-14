51c26fa9a40674d6be1ae9c81c85b87346b75ce9
Suihm <hieunm@rabiloo.com.vn>
2023-07-14 13:24:45 +0700

    init project

---
+++ src/main/java/com/example/translationcsv/TranslationCsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.translationcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/translationcsv/TranslationCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.translationcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
